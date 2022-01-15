17196a5a9ab29c5eaf179b0f2bedeb557ed4072b
TheSnowfield <thesnowfield@moezsoft.com>
2022-01-15 18:29:13 +0800

    move source files

--- src/cqapi/cqapi_can_send_record.c
+++ src/cqapi/cqapi_can_send_record.c
@@ -1,5 +1,5 @@
 #include <cqapi.h>
 
 __cqapi(int32_t) cqapi_can_send_record(int32_t authcode) {
-  
+  return 1;
 }
--- src/cqapi/cqapi_send_private_msg.c
+++ src/cqapi/cqapi_send_private_msg.c
@@ -1,8 +1,7 @@
 #include <cqapi.h>
+#include <stdio.h>
 
 __cqapi(int32_t) cqapi_send_private_msg(int32_t authcode,
   uint64_t account, const char* message) {
-
-
-
+  printf(message);
 }
