124d53e1ad832b379dff6d271656b931b638d4e2
TheSnowfield <thesnowfield@moezsoft.com>
2022-01-15 18:30:11 +0800

    stage files

--- src/main.c
+++ src/main.c
@@ -1,15 +1,13 @@
 #include <stdio.h>
-#include <Windows.h>
-
 #include <cqext.h>
 #include <cqapi.h>
 
 int main(int argc, char** argv) {
 
-  if (argc < 1) return 1;
+  if (argc < 2) return 1;
 
   // Load cq extension dll
-  cq_ext_instance_t instance = cqext_load(argv[0]);
+  cq_ext_instance_t instance = cqext_load(argv[1]);
   if (instance == NULL) return 2;
 
   cq_ret_t result;
@@ -18,7 +16,7 @@ int main(int argc, char** argv) {
   result = cqext_bootstrap(instance, 0x00);
   if (result != 0) return 3;
 
-  // Go enable
+  // Enable extension
   result = cqext_event_enable(instance);
   if (result != 0) return 4;
 
